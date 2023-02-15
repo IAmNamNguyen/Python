9faad1bccf7dd51b61f139bef2f203a0ce06f305
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-02-16 00:31:08 +0700

    Initial commit

---
+++ Bài tập vòng lặp/Bài 1 - BT vòng lặp.py
@@ -0,0 +1,14 @@
+# Cho dãy số vô hạn 12, 15, 18, 21,...
+# Viết chương trình in ra số hạng thứ 2023
+
+#Bộ đếm thứ tự:
+boDem = 0
+
+#Dãy số vô hạn:
+soVohan = 12
+
+while boDem < 2023:
+    boDem + 1
+    soVohan = soVohan + 3
+
+print("Số hạng thứ 2023 trong dãy số vô hạn 12, 15, 18, 21,... là:",soVohan)
