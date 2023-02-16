8bfee32575957e147e3da58dcdc8b682033fdb2e
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-02-16 23:16:47 +0700

    Update Bài 1 - BT vòng lặp.py

--- Bài tập vòng lặp/Bài 1 - BT vòng lặp.py
+++ Bài tập vòng lặp/Bài 1 - BT vòng lặp.py
@@ -1,14 +1,12 @@
 # Cho dãy số vô hạn 12, 15, 18, 21,...
 # Viết chương trình in ra số hạng thứ 2023
 
-#Bộ đếm thứ tự:
-boDem = 0
+#Có công thức tìm số của vị trí số hạng:
+#Số hạng thứ n = (n-1)*Khoảng cách + Số đầu
 
-#Dãy số vô hạn:
-soVohan = 12
+#Ta thấy dãy số vô hạn 12, 15, 18, 21,.. có khoảng cách là 3 
+#Suy ra số hạng thứ 2023 là = (2023-1)*3+12
 
-while boDem < 2023:
-    boDem + 1
-    soVohan = soVohan + 3
+soHang2023 = (2023-1)*3+12
 
-print("Số hạng thứ 2023 trong dãy số vô hạn 12, 15, 18, 21,... là:",soVohan)
+print("Số hạng thứ 2023 của dãy số vô hạn 12, 15, 18, 21,... là:",soHang2023)
