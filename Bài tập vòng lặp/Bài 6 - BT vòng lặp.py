f0069e39a19a4a8c5d4e89a7f53c45781c4da1e2
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-02-18 00:34:24 +0700

    Update Bài 6 - BT vòng lặp.py

---
+++ Bài tập vòng lặp/Bài 6 - BT vòng lặp.py
@@ -0,0 +1,5 @@
+#Viết chương trình phân tích một số nguyên N thành tích các thừa số nguyên tố.
+#VD:	 Nhập N=15 thì kết quả in ra là: 15=3x5
+#             N= 16 thì kết quả in ra là: 16= 2x2x2x2
+
+
