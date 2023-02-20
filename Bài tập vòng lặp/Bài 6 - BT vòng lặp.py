869e2fb2edff3d4f9fb93d34ea2f839538d6ff4a
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-02-20 16:00:37 +0700

    Update Bài 6 - BT vòng lặp.py

--- Bài tập vòng lặp/Bài 6 - BT vòng lặp.py
+++ Bài tập vòng lặp/Bài 6 - BT vòng lặp.py
@@ -2,4 +2,16 @@
 #VD:	 Nhập N=15 thì kết quả in ra là: 15=3x5
 #             N= 16 thì kết quả in ra là: 16= 2x2x2x2
 
+N = int(input("Nhập số nguyên N: "))
+while N<2:
+  N = int(input("Cần số nguyên N lớn hơn 2, xin nhập lại: "))
+
+thuaSoNguyenTo = []
+
+for i in range(2,N+1):
+  while N%i == 0:
+    thuaSoNguyenTo += (i,)
+    N = N/i
+    
+print("N =",N,"gồm tích thừa số nguyên tố là:", thuaSoNguyenTo)
 
