ee7ea19bf0d62ad01d9aabf0b311dedd9b79049e
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-02-22 22:38:38 +0700

    Create Bài 1 - P1.py

---
+++ Bài tập ôn luyện cơ bản/Bài 1 - P1.py
@@ -0,0 +1,10 @@
+# Viết chương trình nhập vào ba số a, b, c, xuất ra màn hình số lớn nhất (max) của ba số đó.
+
+a = float(input("Nhập số a: "))
+b = float(input("Nhập số b: "))
+c = float(input("Nhập số c: "))
+
+num = [a, b, c]
+soLonNhat = max(num)
+
+print("Số lớn nhất trong 3 số a, b, c đã nhập là:",soLonNhat)
