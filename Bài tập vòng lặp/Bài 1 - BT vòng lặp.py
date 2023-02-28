c03888efbdf503f5ab234fd5f1918ed6f9fbe8fd
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-02-28 11:05:18 +0700

    Update Bài 1 - BT vòng lặp.py

--- Bài tập vòng lặp/Bài 1 - BT vòng lặp.py
+++ Bài tập vòng lặp/Bài 1 - BT vòng lặp.py
@@ -10,3 +10,24 @@
 soHang2023 = (2023-1)*3+12
 
 print("Số hạng thứ 2023 của dãy số vô hạn 12, 15, 18, 21,... là:",soHang2023)
+
+#Cách 2:
+# Dãy số này có quy luật là mỗi số hạng bằng số hạng trước đó cộng với 3
+# Ta có thể sử dụng vòng lặp vô hạn while
+
+# Khởi tạo số hạng đầu tiên
+a = 12
+# Khởi tạo biến đếm
+count = 2
+# Sử dụng vòng lặp while để tạo dãy số vô hạn
+while True:
+    # Lấy số hạng trước cộng thêm 3
+    b = a + 3
+    #Test:
+    print(b)
+    # Tăng biến đếm lên một đơn vị
+    count += 1
+    # Kiểm tra nếu biến đếm bằng 2023 thì in ra giá trị của c và thoát khỏi vòng lặp
+    if count == 2023:
+        print(b)
+        break
