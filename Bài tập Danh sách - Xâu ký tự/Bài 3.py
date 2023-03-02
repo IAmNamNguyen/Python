46a68535301678c41d26443042be1bb38633d717
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-03-02 17:35:55 +0700

    Update Bài 3.py

--- Bài tập Danh sách - Xâu ký tự/Bài 3.py
+++ Bài tập Danh sách - Xâu ký tự/Bài 3.py
@@ -5,10 +5,17 @@ B = str(input("Nhập 1 chuỗi ký tự B: "))
 print("Chuỗi ký tự B là: ",B)
 
 # 1. Kiểm tra B có ký tự số hay không?
-for x in range(10):
-    if x in B:
-        print("B có chứa ký tự số")
-        break
-    else:
-        print("B không chứa ký tự số")
-        break
+chuSo = any(x.isnumeric() for x in B)
+if chuSo == True:
+  print("B có chứa ký tự số")
+else:
+  print("B không chứa ký tự số")
+   
+# 2. Kiểm tra B có ký tự in hoa không?
+# Sử dụng hàm any() để duyệt ký tự
+# Sau đó dùng .isupper() để check in hoa
+inHoa = any(x.isupper() for x in B)
+if inHoa == True:
+  print("B có ký tự in hoa")
+else:
+  print("B không có ký tự in hoa")
