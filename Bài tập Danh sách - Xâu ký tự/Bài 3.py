742c8f941e87abf69166709852e2e976f1dec906
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-03-04 11:19:07 +0700

    Update Bài 3.py

--- Bài tập Danh sách - Xâu ký tự/Bài 3.py
+++ Bài tập Danh sách - Xâu ký tự/Bài 3.py
@@ -19,3 +19,26 @@ if inHoa == True:
   print("B có ký tự in hoa")
 else:
   print("B không có ký tự in hoa")
+
+# 3. Nhập vào ký 1 tự C bất kỳ rồi kiểm tra xem có C trong B hay không
+# Nhập 1 ký tự C:
+C = str(input("Nhập MỘT ký tự bất kỳ: "))
+print("Ký tự C bạn đã nhập là:",C) #Xuất C ra màn hình
+#Check C có trong B:
+if C in B:
+  print("Ký tự C có trong chuỗi B")
+else:
+  print("Ký tự C không có trong chuỗi B")
+
+# 4. Kiểm tra B có phải là xâu đối xứng hay không?
+# Ta sử dụng hàm reversed()
+# Hàm reversed() trả về một đối tượng lặp ngược các phần tử của một chuỗi
+r = "".join(reversed(B)) #Nối các phân tử đã lặp ngược lại thành 1 chuỗi
+if B == r: #So sánh đối xứng B với r
+  print("Xâu B:",B,"là xâu đối xứng")
+else:
+  print("Xâu B:",B,"không phải là xâu đối xứng")
+
+# 5. Chuẩn hóa xâu
+# Xâu chuẩn hóa là xâu không có chứa dấu cách ở đầu và cuối câu, 
+# giữa các ký tự chỉ có duy nhất 1 dấu cách
