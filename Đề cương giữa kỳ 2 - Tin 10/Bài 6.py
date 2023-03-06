c2f7aa41787db99216be6b23c6a48b74da1f187e
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-03-06 18:18:56 +0700

    Update Bài 6.py

--- Đề cương giữa kỳ 2 - Tin 10/Bài 6.py
+++ Đề cương giữa kỳ 2 - Tin 10/Bài 6.py
@@ -2,17 +2,27 @@
 # in ra các số lớn hơn 3 và nhỏ hơn 6.
 
 #Nhập N số nguyên trong dãy
-N = int(input("Nhập số phần tử N số nguyên mà bạn muốn sẽ có trong dãy số: "))
+N = int(input("Nhập số phần tử muốn có trong dãy số: "))
 #Tạo 1 danh sách rỗng
 daySo = []
-#Tạo vòng lặp với x lần tương ứng với số phần từ muốn có trong dãy:
+#Tạo vòng lặp với N lần tương ứng với số phần tử muốn có trong dãy:
 for x in range(N):
-    So = int(input("Nhập 1 số bất kỳ thứ " + str(x+1)+": "))
-    daySo.append(So)
+    daySo.append(int(input("Nhập 1 số bất kỳ thứ " + str(x+1)+": ")))
 
-thoaManDeBai = []
+lonHonBa = []
+beHonSau = []
 for x in daySo:
-    if 3 < x < 6:
-        thoaManDeBai.append(x)
+    if x>3:
+        lonHonBa.append(x)
+    if x<6:
+        beHonSau.append(x)
 
-print("Các số lớn hơn 3 và bé hơn 6 là:",thoaManDeBai)
+if lonHonBa == []:
+    print("Không có số lớn hơn 3.")
+else:
+    print("Các số lớn hơn 3 là:",lonHonBa)
+if beHonSau == []:
+    print("Không có số bé hơn 6.")
+else:
+    print("Các số bé hơn 6 là:",beHonSau)
+
