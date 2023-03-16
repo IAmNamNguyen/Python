fed1ee1e7216266974f471dc2420afcaa6383236
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-03-16 16:24:40 +0700

    Update Bài 6.py

--- Đề cương giữa kỳ 2 - Tin 10/Bài 6.py
+++ Đề cương giữa kỳ 2 - Tin 10/Bài 6.py
@@ -8,21 +8,9 @@ daySo = []
 #Tạo vòng lặp với N lần tương ứng với số phần tử muốn có trong dãy:
 for x in range(N):
     daySo.append(int(input("Nhập 1 số bất kỳ thứ " + str(x+1)+": ")))
-
-lonHonBa = []
-beHonSau = []
+so = []
 for x in daySo:
-    if x>3:
-        lonHonBa.append(x)
-    if x<6:
-        beHonSau.append(x)
-
-if lonHonBa == []:
-    print("Không có số lớn hơn 3.")
-else:
-    print("Các số lớn hơn 3 là:",lonHonBa)
-if beHonSau == []:
-    print("Không có số bé hơn 6.")
-else:
-    print("Các số bé hơn 6 là:",beHonSau)
+    if 3 < x < 6:
+        so.append(x)
+print(so)
 
